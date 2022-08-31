4eb08d67569606144beda1cf9591f26140065dce
rose-vincent <rose_vincent@DESKTOP-92R2BCF.localdomain>
2022-08-31 10:14:24 +0400

    added height and weight to bmi.py

--- bmicalc/bmi.py
+++ bmicalc/bmi.py
@@ -1,3 +1,39 @@
+def weight_cal():
+    '''
+    To define your weight in SI Units to calculate BMI
+    Input(s): By User through prompt
+    Output(s): Weight in kilogram
+    '''
+    unit_w = input("Is your weight in kilograms or pounds? Please enter k or p: ")
+    if unit_w == "p" or unit_w == "k":
+        weight = float(input("Please enter your weight: "))
+        if unit_w == "p":
+            weight *= 0.453592
+        elif unit_w == "k":
+            weight = weight
+        print("Your weight is {:.2f} kg")
+        return weight
+    else:
+        print("Please try again, thank you.")
+
+def height_cal():
+    '''
+    To define your height in SI Units to calculate BMI
+    Input(s): By User through prompt
+    Output(s): Height in metre
+    '''
+    unit_h = input("Is your height in centimeters or feet? Please enter m or ft: ")
+    if unit_h == "m" or unit_h == "ft":
+        height = float(input("Please enter your height: "))
+        if unit_h == "ft":
+            height *= 0.3048
+        elif unit_h == "m":
+            height = height
+        print("Your height is {:.2f} m")
+        return height
+    else:
+        print("Please try again, thank you.")
+
 def bmi_cal(weight,height):
     bmi = weight/(height**2)
     print("\nYour body mass index (BMI) is {:.2f}".format(bmi))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='bmicalculator',
+	name='bmicalc',
 	version='1.0.0',
 	packages=find_packages()
 )
